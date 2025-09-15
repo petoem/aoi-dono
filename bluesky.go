031e29316669e6df8ad6c5e34ce9a606aff6ad30
Michael Petö <michael@petoe.me>
2025-09-15 11:27:26 +0200

    Automatically linkify bluesky post text

--- bluesky.go
+++ bluesky.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/url"
 
 	"github.com/bluesky-social/indigo/api/atproto"
 	"github.com/bluesky-social/indigo/api/bsky"
 	"github.com/bluesky-social/indigo/atproto/client"
 	"github.com/bluesky-social/indigo/atproto/syntax"
 	"github.com/bluesky-social/indigo/lex/util"
+	"mvdan.cc/xurls/v2"
 )
 
 func blueskyPost(ctx context.Context, credentials Bluesky, language, postContent string) (string, error) {
@@ -17,9 +19,12 @@ func blueskyPost(ctx context.Context, credentials Bluesky, language, postContent
 		return "", err
 	}
 
+	facetsLink := findRichtextFacetLinks(postContent)
+
 	record := bsky.FeedPost{
 		CreatedAt: string(syntax.DatetimeNow()),
 		Langs:     []string{language},
+		Facets:    facetsLink,
 		Text:      postContent,
 	}
 
@@ -37,4 +42,33 @@ func blueskyPost(ctx context.Context, credentials Bluesky, language, postContent
 	// at://<DID>/<COLLECTION>/<RKEY>
 	// https://bsky.app/profile/<DID>/post/<RKEY>
 	return response.Uri, nil
+}
+
+func findRichtextFacetLinks(text string) []*bsky.RichtextFacet {
+	facets := make([]*bsky.RichtextFacet, 0)
+	ru := xurls.Relaxed()
+	locs := ru.FindAllStringIndex(text, -1)
+	for _, loc := range locs {
+		uri, err := url.Parse(text[loc[0]:loc[1]])
+		if err != nil {
+			continue // skip this uri
+		}
+		if uri.Scheme == "" {
+			uri.Scheme = "https"
+		}
+		facets = append(facets, &bsky.RichtextFacet{
+			Features: []*bsky.RichtextFacet_Features_Elem{
+				{
+					RichtextFacet_Link: &bsky.RichtextFacet_Link{
+						Uri: uri.String(),
+					},
+				},
+			},
+			Index: &bsky.RichtextFacet_ByteSlice{
+				ByteEnd:   int64(loc[1]),
+				ByteStart: int64(loc[0]),
+			},
+		})
+	}
+	return facets
 }
